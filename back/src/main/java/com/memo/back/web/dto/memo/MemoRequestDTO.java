9b36fe066135199219d036e959b0f7e7c98a0748
namung08 <namung08@gmail.com>
2025-05-13 16:31:22 +0900

    :sparkles: feat: MemoRequestDTO에 유효성 검증 어노테이션 추가

--- back/src/main/java/com/memo/back/web/dto/memo/MemoRequestDTO.java
+++ back/src/main/java/com/memo/back/web/dto/memo/MemoRequestDTO.java
@@ -1,7 +1,15 @@
 package com.memo.back.web.dto.memo;
 
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * 메모의 생성 및 수정을 위해 사용할 request dto
+ * @param title 메모의 제목
+ * @param content 메모의 내용
+ */
 public record MemoRequestDTO(
     String title,
+    @NotBlank(message = "메모의 내용을 필수 사항입니다.")
     String content
 ) {
 }
