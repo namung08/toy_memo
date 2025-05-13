3c24c75008eef0d65f96cf4db487c132a6298e80
namung08 <namung08@gmail.com>
2025-05-13 16:49:24 +0900

    :sparkles: feat: 메모를 수정하는 메서드 작성

--- back/src/main/java/com/memo/back/memo/repository/MemoRepository.java
+++ back/src/main/java/com/memo/back/memo/repository/MemoRepository.java
@@ -2,6 +2,13 @@ package com.memo.back.memo.repository;
 
 import com.memo.back.memo.model.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+  @Transactional
+  @Modifying
+  @Query("update Memo m set m.title = ?1, m.content = ?2 where m.id = ?3")
+  int updateTitleAndContentById(String title, String content, Long id);
 }
