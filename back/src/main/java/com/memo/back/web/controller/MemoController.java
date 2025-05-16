881af4501b56961180dc1f60842ea39b2ae3cfe0
namung08 <namung08@gmail.com>
2025-05-16 15:51:17 +0900

    :sparkles: feat: cors 설정 추가

--- back/src/main/java/com/memo/back/web/controller/MemoController.java
+++ back/src/main/java/com/memo/back/web/controller/MemoController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/memo")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173") // Vite 기본 포트
 public class MemoController {
   private final MemoService service;
 
