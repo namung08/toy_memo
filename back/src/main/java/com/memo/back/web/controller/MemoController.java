1ac9ec690e1b982cd7fe949ec73a1149a59a41b5
namung08 <namung08@gmail.com>
2025-05-19 16:42:25 +0900

    :fire: perf: cors annotation 삭제

--- back/src/main/java/com/memo/back/web/controller/MemoController.java
+++ back/src/main/java/com/memo/back/web/controller/MemoController.java
@@ -14,7 +14,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/memo")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173") // Vite 기본 포트
 public class MemoController {
   private final MemoService service;
 
